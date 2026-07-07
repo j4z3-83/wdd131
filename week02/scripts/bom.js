7c47ab5ec49fcaf2079741e159dc45f4354fc1f3
j4z3-83 <jaldridge@byupathway.edu>
2026-07-07 13:53:20 -0600

    WK2 TEST

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,31 +2,36 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
+let listLength = document.querySelectorAll('ul#list id li').length;
 
-// handle button clicks
-button.addEventListener("click", function () {
-	if (input.value != "") {
-		// create list item 
-		const li = document.createElement("li");
-		li.textContent = input.value;
-		// create a  delete button
-		const delButton = document.createElement("button");
-		delButton.textContent = "❌";
-		delButton.addEventListener("click", function () {
-			list.removeChild(li);
-			input.focus();
-		});
-		// add the delete button to the list item
-		li.appendChild(delButton);
-		// display the completed list item
-		list.appendChild(li);
-		// clear the input box
-		input.value = "";
-	}
-	input.focus();
-
-
-});
+if(listLength == 10){
+    button = document.getElementById("button");
+    button.disabled = true;
+}
+else{
+    // handle button clicks
+    button.addEventListener("click", function () {
+        if (input.value != "") {
+            // create list item 
+            const li = document.createElement("li");
+            li.textContent = input.value;
+            // create a  delete button
+            const delButton = document.createElement("button");
+            delButton.textContent = "❌";
+            delButton.addEventListener("click", function () {
+                list.removeChild(li);
+                input.focus();
+            });
+            // add the delete button to the list item
+            li.appendChild(delButton);
+            // display the completed list item
+            list.appendChild(li);
+            // clear the input box
+            input.value = "";
+        }
+    input.focus();  
+    });
+}
 
 
 
