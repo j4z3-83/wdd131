04501c0de02ac9e3cf56cec08960c65e10f2c057
j4z3-83 <jaldridge@byupathway.edu>
2026-07-07 12:15:07 -0600

    js update

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,28 +1,31 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
+// select elements from the DOM
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
 
-// handle button clicks
-button.addEventListener("click", function () {
+// wait for button clicks
+buttonElement.addEventListener("click", function () {
 	// Check if the user entered something
-	if (input.value != "") {
+	if (inputElement.value != "") {
 		// create list item and give it the value of the input
 		const li = document.createElement("li");
-		li.textContent = input.value;
+		li.textContent = inputElement.value;
 		// create a button and add a click event listener
 		const deleteBtn = document.createElement("button");
-		delButton.textContent = "❌";
-		delButton.addEventListener("click", function () {
-			list.removeChild(li);
-			input.focus();
+		deleteBtn.textContent = "❌";
+		deleteBtn.addEventListener("click", function () {
+			listElement.removeChild(li);
+			inputElement.focus();
 		});
 		// add the button to the list item
-		li.appendChild(delButton);
-		// display the list item in the list
-		// clear the user input box
-		input.value = "";
+		li.appendChild(deleteBtn);
+		// OUTPUT: finally display the completed list item to the unordered list
+		listElement.appendChild(li);
+		// clear the user input field
+		inputElement.value = "";
 	}
-	input.focus();
+	// focus the user back to the input field
+	inputElement.focus();
 });
 
 
