89313a4bbced4af20edad0ba814e90888806cbeb
j4z3-83 <jaldridge@byupathway.edu>
2026-07-07 12:00:05 -0600

    wk2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,14 +2,15 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-// wait for button clicks
+// handle button clicks
 button.addEventListener("click", function () {
 	// Check if the user entered something
-	if (input.value != "" && list.length <= 10) {
-		// create list item and give it the value of the input
+	if (input.value != "") 
+    {
+		// create list item
 		const li = document.createElement("li");
 		li.textContent = input.value;
-		// create a button and add a click event listener
+		// create a button 
 		const deleteBtn = document.createElement("button");
 		delButton.textContent = "❌";
 		delButton.addEventListener("click", function () {
@@ -18,10 +19,9 @@ button.addEventListener("click", function () {
 		});
 		// add the button to the list item
 		li.appendChild(delButton);
-		// OUTPUT: finally display the completed list item to the unordered list
+		// add the list item to the list
 		list.appendChild(li);
-		// clear the user input field
-		input.value = "";
+		// clear the user input box
 	}
 	input.focus();
 });
